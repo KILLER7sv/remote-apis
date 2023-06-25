5abfe0b6decd7dbaa2b53d24728f5faa012c618a
KILLER7sv <116873144+KILLER7sv@users.noreply.github.com>
2023-06-25 20:38:05 +0530

    Start

---
+++ src/main/java/com/example/remoteApis/RemoteAliController.java
@@ -0,0 +1,10 @@
+package com.example.remoteApis;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/remote")
+public class RemoteAliController {
+
+}
---
+++ src/test/java/com/example/remoteApis/RemoteApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.remoteApis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
